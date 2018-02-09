99862c76ee7e24d2e4359ee5337a5d9c9b2febae
Vincent <gw0683@163.com>
2018-02-09 17:51:43 +0800

    my website django project

--- mywebsite/comments/views.py
+++ mywebsite/comments/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def addComments(request):
     return render(request, 'comments/addComments.html')
+
+
+def saveComments(request):
+    pass
 
 
 def listComments(request):
