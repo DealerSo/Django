4ca2459fe4fc176a703a0efe399986a5d9e64f7e
Vincent <gw0683@163.com>
2018-02-11 17:04:40 +0800

    my website django project

--- mywebsite/comments/templatetags/mask.py
+++ mywebsite/comments/templatetags/mask.py
@@ -1,9 +1,12 @@
 ######################自定义过滤器，给到template使用
 from django import template
 
+# 为了成为一个可用的标签库，这个模块必须包含一个名为 register的变量，
+# 它是template.Library 的一个实例，所有的标签和过滤器都是在其中注册的
 register = template.Library()
 
-@register.filter
+# 进行注册,注册以后就可以在template中使用了
+@register.filter()
 def maskPhone(phone,interval):
     start,end = str(interval).split("-");
     # 转为int型
